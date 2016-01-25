f60edb0998d4c3d3bca3c63f2fdd1da4ecd41503
bananasplitter <tiana_pehle@yahoo.com>
2016-01-25 14:45:50 -0800

    collatz

---
+++ CollatzConjecture/CollatzConjecture/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollatzConjecture
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number here greater than 1");
+
+            string input = Console.ReadLine();
+
+            int yourNumber = int.Parse(input);
+
+            int currentStep = 0;
+
+            int largestStep = 0;
+
+            int largestNumber = yourNumber;
+
+            while (yourNumber != 1)
+
+            {
+                currentStep++;
+
+                if (yourNumber > largestNumber);
+                {
+                    yourNumber = largestNumber
+                    Console.WriteLine(yourNumber);
+                }
+                    if (yourNumber % 2 == 0)
+                    {
+                        yourNumber = yourNumber / 2;
+
+                        Console.WriteLine(yourNumber);
+                    }
+
+                    else
+
+
+                    {
+                        yourNumber = yourNumber * 3 + 1;
+
+                        Console.WriteLine(yourNumber);
+                    }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
